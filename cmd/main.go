89422b1577e5421380584f425802afd7a2194bc0
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-09 17:01:36 +0200

    change functions on handlefunc

--- cmd/main.go
+++ cmd/main.go
@@ -40,13 +40,13 @@ func main(){
 
 	r.HandleFunc("/api/post/",handlers.GetAllPost).Methods("GET")
 	r.HandleFunc("/api/post/",handlers.CreatePost).Methods("POST")
-	r.HandleFunc("/api/post/{id}",handlers.GetAllPost).Methods("GET")
+	r.HandleFunc("/api/post/{id}",handlers.GetOnePost).Methods("GET")
 	r.HandleFunc("/api/post/{id}",handlers.DeletePost).Methods("DELETE")
 	r.HandleFunc("/api/post/{id}",handlers.UpdatePost).Methods("PUT")
 
 	r.HandleFunc("/api/comment/",handlers.GetAllComment).Methods("GET")
 	r.HandleFunc("/api/comment/",handlers.CreateComment).Methods("POST")
-	r.HandleFunc("/api/comment/{id}",handlers.GetAllComment).Methods("GET")
+	r.HandleFunc("/api/comment/{id}",handlers.GetOneComment).Methods("GET")
 	r.HandleFunc("/api/comment/{id}",handlers.DeleteComment).Methods("DELETE")
 	r.HandleFunc("/api/comment/{id}",handlers.UpdateComment).Methods("PUT")
 
