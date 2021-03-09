e5fbdd5da422253de08fce7b7aca98602ca84ec7
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-09 16:59:23 +0200

    add handle function to route crud operations with comments

--- cmd/main.go
+++ cmd/main.go
@@ -44,6 +44,12 @@ func main(){
 	r.HandleFunc("/api/post/{id}",handlers.DeletePost).Methods("DELETE")
 	r.HandleFunc("/api/post/{id}",handlers.UpdatePost).Methods("PUT")
 
+	r.HandleFunc("/api/comment/",handlers.GetAllComment).Methods("GET")
+	r.HandleFunc("/api/comment/",handlers.CreateComment).Methods("POST")
+	r.HandleFunc("/api/comment/{id}",handlers.GetAllComment).Methods("GET")
+	r.HandleFunc("/api/comment/{id}",handlers.DeleteComment).Methods("DELETE")
+	r.HandleFunc("/api/comment/{id}",handlers.UpdateComment).Methods("PUT")
+
 	log.Fatal(http.ListenAndServe(":8080",r))
 
 }
