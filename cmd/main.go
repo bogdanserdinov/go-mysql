6bedfc3bf59bc22c566b41e5bd09eb551181c758
bohdan_serdinov <bogdan.serdinov.2002@gmail.com>
2021-03-12 17:08:10 +0200

    changed routing in main func to echo

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"awesomeProject/nix/pkg/handlers"
 	"github.com/gorilla/mux"
-	"log"
-	"net/http"
+	"github.com/labstack/echo"
 )
 
 func main(){
@@ -12,7 +11,7 @@ func main(){
 
 	r.HandleFunc("/api/get/",handlers.GetDate)
 
-	r.HandleFunc("/api/post/",handlers.GetAllPost).Methods("GET")
+	/*r.HandleFunc("/api/post/",handlers.GetAllPost).Methods("GET")
 	r.HandleFunc("/api/post/",handlers.CreatePost).Methods("POST")
 	r.HandleFunc("/api/post/{id}",handlers.GetOnePost).Methods("GET")
 	r.HandleFunc("/api/post/{id}",handlers.DeletePost).Methods("DELETE")
@@ -22,7 +21,21 @@ func main(){
 	r.HandleFunc("/api/comment/",handlers.CreateComment).Methods("POST")
 	r.HandleFunc("/api/comment/{id}",handlers.GetOneComment).Methods("GET")
 	r.HandleFunc("/api/comment/{id}",handlers.DeleteComment).Methods("DELETE")
-	r.HandleFunc("/api/comment/{id}",handlers.UpdateComment).Methods("PUT")
+	r.HandleFunc("/api/comment/{id}",handlers.UpdateComment).Methods("PUT")*/
 
-	log.Fatal(http.ListenAndServe(":8080",r))
+	e := echo.New()
+
+	e.GET("/api/post/",handlers.GetAllPost)
+	e.POST("/api/post/",handlers.CreatePost)
+	e.GET("/api/post/{id}",handlers.GetOnePost)
+	e.DELETE("/api/post/{id}",handlers.DeletePost)
+	e.PUT("/api/post/{id}",handlers.UpdatePost)
+
+	e.GET("/api/comment/",handlers.GetAllComment)
+	e.POST("/api/comment/",handlers.CreateComment)
+	e.GET("/api/comment/{id}",handlers.GetOneComment)
+	e.DELETE("/api/comment/{id}",handlers.DeleteComment)
+	e.PUT("/api/comment/{id}",handlers.UpdateComment)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
